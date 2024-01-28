e6cb4b33998126a101c0aaa9d0378a663e3ad22c
HuParry <huparry@outlook.com>
2024-01-28 10:59:08 +0800

    更改数据存储库为aiosqlite

--- nonebot_plugin_randpic/__init__.py
+++ nonebot_plugin_randpic/__init__.py
@@ -1,4 +1,3 @@
-
 from httpx import AsyncClient
 
 from nonebot.adapters.onebot.v11 import MessageSegment, Message, Event
@@ -13,7 +12,7 @@ from nonebot import get_driver
 from nonebot.log import logger
 import asyncio
 import hashlib
-import sqlite3
+import aiosqlite
 from nonebot.params import Fullmatch
 
 from .config import Config
@@ -27,12 +26,13 @@ __plugin_meta__ = PluginMetadata(
     config=Config,
     supported_adapters={"nonebot.adapters.onebot.v11"},
 )
+
 config_dict = Config.parse_obj(get_driver().config.dict())
 randpic_command_list: List[str] = config_dict.randpic_command_list
 
 randpic_command_set: Set[str] = set(randpic_command_list)
 
-randpic_command_tuple: Tuple[str,...] = tuple(randpic_command_set)  # 形成指令元组
+randpic_command_tuple: Tuple[str, ...] = tuple(randpic_command_set)  # 形成指令元组
 randpic_command_add_tuple = tuple("添加" + tup for tup in randpic_command_tuple)  # 形成添加指令元组
 
 randpic_path = Path(config_dict.randpic_store_dir_path)
@@ -41,7 +41,7 @@ randpic_command_path_tuple = tuple(randpic_path / command for command in randpic
 hash_str = 'vtw3srzmcn0vqp_'
 randpic_filename: str = 'randpic_{command}_{index}'
 
-connection: sqlite3.Connection
+connection: aiosqlite.Connection
 
 # 激活驱动器
 driver = get_driver()
@@ -50,8 +50,14 @@ driver = get_driver()
 @driver.on_startup
 async def _():
     logger.info("正在检查文件...")
-    asyncio.create_task(create_file())
+    await asyncio.create_task(create_file())
     logger.info("文件检查完成，欢迎使用插件！")
+
+
+@driver.on_shutdown
+async def close_connection():
+    logger.info("正在关闭数据库")
+    await connection.close()
 
 
 # 创建所需文件夹和数据库
@@ -64,18 +70,19 @@ async def create_file():
     # 创建数据库，没有数据表创建数据表
 
     global connection
-    connection = sqlite3.connect(randpic_path / "data.db")
-    cursor = connection.cursor()
+    connection = await aiosqlite.connect(randpic_path / "data.db")
+    cursor = await connection.cursor()
+
     # 创建表
     for command in randpic_command_tuple:
-        cursor.execute('DROP table if exists Pic_of_{command};'.format(command=command))
-        cursor.execute('''
+        await cursor.execute('DROP table if exists Pic_of_{command};'.format(command=command))
+        await cursor.execute('''
             CREATE TABLE IF NOT EXISTS Pic_of_{command} (
                 md5 TEXT PRIMARY KEY,
                 img_url TEXT
             )
             '''.format(command=command))
-        connection.commit()
+        await connection.commit()
 
     # 读取所有文件夹文件，调整文件夹内图片，并写入数据库
     for index in range(len(randpic_command_path_tuple)):
@@ -86,8 +93,9 @@ async def create_file():
         for i in range(len(randpic_file_list)):
             filename = randpic_file_list[i]
             filename_without_extension, filename_extension = os.path.splitext(filename)
-            hash_new_filename = hash_str+randpic_filename.format(command=randpic_command_tuple[index],
-                                                   index=str(i+1))+(filename_extension if filename_extension != '' else '.jpg')
+            hash_new_filename = hash_str + randpic_filename.format(command=randpic_command_tuple[index],
+                                                                   index=str(i + 1)) + (
+                                    filename_extension if filename_extension != '' else '.jpg')
             os.rename(path / filename, path / hash_new_filename)
 
         # 将哈希化的文件名订正为规范名
@@ -97,7 +105,6 @@ async def create_file():
             new_filename = hash_filename.replace(hash_str, '')
             os.rename(path / hash_filename, path / new_filename)
 
-
         # 将图片信息写入数据库
         randpic_file_list = os.listdir(path)
         for i in range(len(randpic_file_list)):
@@ -106,12 +113,12 @@ async def create_file():
                 data = f.read()
             fmd5 = hashlib.md5(data).hexdigest()
 
-            cursor = connection.cursor()
+            cursor = await connection.cursor()
             command: str = randpic_command_tuple[index]
-            cursor.execute(
+            await cursor.execute(
                 'INSERT or REPLACE INTO Pic_of_{command}(md5, img_url) VALUES (?, ?)'.format(command=command),
                 (fmd5, str(Path() / command / filename)))
-            connection.commit()
+            await connection.commit()
 
 
 picture = on_fullmatch(randpic_command_tuple, permission=GROUP, priority=1, block=True)
@@ -120,17 +127,18 @@ picture = on_fullmatch(randpic_command_tuple, permission=GROUP, priority=1, bloc
 @picture.handle()
 async def pic(event: Event):
     global connection
-    cursor = connection.cursor()
+    cursor = await connection.cursor()
     command = str(event.get_message()).strip()
-    cursor.execute(f'SELECT img_url FROM Pic_of_{command} ORDER BY RANDOM() limit 1')
-    data = cursor.fetchone()
+    await cursor.execute(f'SELECT img_url FROM Pic_of_{command} ORDER BY RANDOM() limit 1')
+    data = await cursor.fetchone()
     if data is None:
         await picture.finish('当前还没有图片!')
     file_name = data[0]
     img = randpic_path / file_name
     try:
         await picture.send(MessageSegment.image(img))
-    except:
+    except Exception as e:
+        logger.info(e)
         await picture.send(f'{command}出不来了，稍后再试试吧~')
 
 
@@ -140,23 +148,24 @@ add = on_fullmatch(randpic_command_add_tuple, permission=GROUP_ADMIN | GROUP_OWN
 @add.got("pic", prompt="请发送图片！")
 async def add_pic(args: str = Fullmatch(), pic_list: Message = Arg('pic')):
     global connection
-    cursor = connection.cursor()
+    cursor = await connection.cursor()
     command = args.replace('添加', '')
 
-    for pic in pic_list:
-        if pic.type != 'image':
-            await add.send(pic + MessageSegment.text("\n输入格式有误，请重新触发指令！"), at_sender=True)
+    for pic_name in pic_list:
+        if pic_name.type != 'image':
+            await add.send(pic_name + MessageSegment.text("\n输入格式有误，请重新触发指令！"), at_sender=True)
             continue
-        pic_url = pic.data['url']
+        pic_url = pic_name.data['url']
 
         async with AsyncClient() as client:
             resp = await client.get(pic_url, timeout=5.0)
 
         try:
             resp.raise_for_status()
-        except:
+        except Exception as e:
+            logger.warning(e)
             await add.send(
-                pic +
+                pic_name +
                 MessageSegment.text('\n保存出错了，这张请重试')
             )
             continue
@@ -164,23 +173,24 @@ async def add_pic(args: str = Fullmatch(), pic_list: Message = Arg('pic')):
         data = resp.content
         fmd5 = hashlib.md5(data).hexdigest()
 
-        cursor.execute(f'SELECT img_url FROM Pic_of_{command} where md5=?', (fmd5,))
-        status = cursor.fetchone()
+        await cursor.execute(f'SELECT img_url FROM Pic_of_{command} where md5=?', (fmd5,))
+        status = await cursor.fetchone()
 
         if status is not None:
-            await add.send(pic + Message('\n这张已经有了，不能重复添加！') )
+            await add.send(pic_name + Message('\n这张已经有了，不能重复添加！'))
         else:
             without_extension, extension = os.path.splitext(pic_url)
-            randpic_cur_picnum = len(os.listdir(randpic_path/command))
-            file_name = (randpic_filename.format(command=command, index=str(randpic_cur_picnum+1))
-                        +(extension if extension != '' else '.jpg'))
-            file_path = randpic_path/ command / file_name
+            randpic_cur_picnum = len(os.listdir(randpic_path / command))
+            file_name = (randpic_filename.format(command=command, index=str(randpic_cur_picnum + 1))
+                         + (extension if extension != '' else '.jpg'))
+            file_path = randpic_path / command / file_name
 
             try:
                 with file_path.open("wb") as f:
                     f.write(data)
-                cursor.execute('insert into Pic_of_{command}(md5, img_url) values (?, ?)'.format(command=command),
-                               (fmd5, str(Path() / command / file_name)))
-                await add.send(pic + Message("\n导入成功！"), at_sender=True)
-            except:
-                await add.send(pic + Message("\n导入失败！"), at_sender=True)
+                await cursor.execute('insert into Pic_of_{command}(md5, img_url) values (?, ?)'.format(command=command),
+                                     (fmd5, str(Path() / command / file_name)))
+                await add.send(pic_name + Message("\n导入成功！"), at_sender=True)
+            except Exception as e:
+                logger.warning(e)
+                await add.send(pic_name + Message("\n导入失败！"), at_sender=True)
--- test.py
+++ test.py
@@ -1,3 +1,28 @@
-from pathlib import Path
+from nonebot.adapters import Event
+from nonebot.params import Depends
+from nonebot import require, on_message
+from sqlalchemy.orm import Mapped, mapped_column
 
-print(Path("./data/sfsfd"))
+require("nonebot_plugin_orm")
+from nonebot_plugin_orm import Model, async_scoped_session
+
+matcher = on_message()
+
+
+def get_user_id(event: Event) -> str:
+    return event.get_user_id()
+
+
+class User(Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str] = Depends(get_user_id)
+
+
+@matcher.handle()
+async def _(event: Event, sess: async_scoped_session, user: User | None):
+    if user:
+        await matcher.finish(f"Hello, {user.user_id}")
+
+    sess.add(User(user_id=get_user_id(event)))
+    await sess.commit()
+    await matcher.finish("Hello, new user!")

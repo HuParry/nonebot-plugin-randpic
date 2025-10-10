4498330a3d0313f433a65aab4849181e84fb9632
HuParry <hu_parry@qq.com>
2025-10-10 18:53:05 +0800

    v1.0.4:图片加载采用懒加载模式，避免流量浪费

--- nonebot_plugin_randpic/web.py
+++ nonebot_plugin_randpic/web.py
@@ -320,7 +320,8 @@ class StaticImageGalleryGenerator:
             <img src="{image}" 
                  alt="{image}" 
                  class="image-preview"
-                 onerror="this.style.display='none'; this.nextElementSibling.style.display='block'; loading='lazy'">
+                 onerror="this.style.display='none'; this.nextElementSibling.style.display='block';"
+                 loading="lazy">
             <div style="display:none; padding: 20px; text-align: center; color: #666;">
                 图片无法显示
             </div>

3e71ad7aac751d969f0682bf9a2772209a12b5aa
HuParry <hu_parry@qq.com>
2026-05-14 15:56:41 +0800

    修复驱动器中的bug

--- nonebot_plugin_randpic/drivers/fastapi.py
+++ nonebot_plugin_randpic/drivers/fastapi.py
@@ -5,9 +5,9 @@ from fastapi.staticfiles import StaticFiles
 from nonebot.drivers.fastapi import Driver
 
 
-def register_route(driver: Driver, randpic_puclic_path: Path):
+def register_route(driver: Driver, randpic_public_path: Path):
     app = driver.server_app
 
-    static_path = str((randpic_puclic_path).resolve())
+    static_path = str(randpic_public_path.resolve())
 
     app.mount("/randpic", StaticFiles(directory=static_path, html=True), name="randpic")
